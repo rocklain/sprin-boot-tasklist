f0f91be29c52fcebc78ca37039876ecbc0a502ec
ryoma <ztgk.3142@gmail.com>
2024-12-29 16:43:16 +0900

    fix_削除ボタン押下時のポップアップ後をＰＯＳＴリクエストに修正

--- src/main/java/jp/gihyo/pro/java/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/pro/java/tasklist/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -61,8 +62,8 @@ public class HomeController {
         return "redirect:/list";
     }
 
-    @GetMapping("/delete")
-    String deleteItem(@RequestParam("id") String id){
+    @PostMapping("/delete")
+    String deleteItem(@RequestParam("id") String id) {
         dao.delete(id);
         return "redirect:/list";
     }
